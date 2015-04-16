9d981b3a7f5384f576e6ef9218af030e84140619
Odysseas <OdysseasKr@users.noreply.github.com>
2015-04-17 02:10:03 +0300

    Added linear algebra section. Added array operations

---
+++ LinearAlgebra/arrayOperations.hs
@@ -0,0 +1,29 @@
+---
+--- Array operations by Odysseas (16/04/2015)
+---
+
+--- Addition
+add :: (Num a) => [a] -> [a] -> [a]
+add [] [] = []
+add (x:xs) (y:ys) =
+    (x+y) : (add xs ys)
+add _ _ = error "Invalid arguments"
+
+--- Subtraction
+sub :: (Num a) => [a] -> [a] -> [a]
+sub [] [] = []
+sub (x:xs) (y:ys) =
+    (x-y) : (sub xs ys)
+sub _ _ = error "Invalid arguments"
+
+--- Multiplication
+multi :: (Num a) => a -> [a] -> [a]
+multi x y = map (*x) y
+
+--- Division
+division :: (Fractional a) => a -> [a] -> [a]
+division x y = map (/x) y
+
+--- Power
+pow :: (Integral a, Num b) => a -> [b] -> [b]
+pow x y = map (^x) y
